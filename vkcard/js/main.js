78510debbe7a9a07a78b04a761e6d61da494fe13
vinilzen <marchukilya@gmail.com>
2013-02-07 21:18:18 +0100

    onclick remove

--- vkcard/js/main.js
+++ vkcard/js/main.js
@@ -183,10 +183,10 @@ $(function(){
 
     for (var i=0; i<frCount; i++) {
       onlineStr += '<li>'+
-                      '<span onclick="select_user('+i+'); return false;" class="btn btn-block" data-i="'+i+'" data-value="' + data.response[i].uid + '" data-photo="'+data.response[i].photo+'">'
+                      '<span class="btn btn-block" data-i="'+i+'" data-value="' + data.response[i].uid + '" data-photo="'+data.response[i].photo+'">'
                          + data.response[i].first_name + ' ' + data.response[i].last_name + 
                       '</span>'+
-                    '</li>';
+                    '</li>'; // onclick="select_user('+i+'); return false;" 
     }
     $('#vk_auth').html(onlineStr);
   });
