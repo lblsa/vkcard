baa35ee1ec3a70a55946afdb4506049297d0d6f6
vinilzen <marchukilya@gmail.com>
2013-02-14 21:15:54 +0100

    test imagick error

---
+++ t.php
@@ -0,0 +1,47 @@
+<?php
+/* Чтение изображения */
+
+$im = new Imagick("files/medium/Masha%20i%20Marina_1360848581.jpg");
+
+echo $im; die;
+
+/* Миниатюра изображения */
+$im->thumbnailImage(200, null);
+
+/* Создание рамки для изображения */
+$im->borderImage(new ImagickPixel("white"), 5, 5);
+
+/* Клонируем изображение и зеркально поворачиваем его */
+$reflection = $im->clone();
+$reflection->flipImage();
+
+/* Создаём градиент. Это будет наложением для отражения */
+$gradient = new Imagick();
+
+/* Градиент должен быть достаточно большой для изображения и его рамки */
+$gradient->newPseudoImage($reflection->getImageWidth() + 10, $reflection->getImageHeight() + 10, "gradient:transparent-black");
+
+/* Наложение градиента на отражение */
+$reflection->compositeImage($gradient, imagick::COMPOSITE_OVER, 0, 0);
+
+/* Добавляем прозрачность. Требуется ImageMagick 6.2.9 или выше */
+$reflection->setImageOpacity( 0.3 );
+
+/* Создаём пустой холст */
+$canvas = new Imagick();
+
+/* Холст должен быть достаточно большой, чтобы вместить оба изображения */
+$width = $im->getImageWidth() + 40;
+$height = ($im->getImageHeight() * 2) + 30;
+$canvas->newImage($width, $height, new ImagickPixel("black"));
+$canvas->setImageFormat("png");
+
+/* Наложение оригинального изображения и отражения на холст */
+$canvas->compositeImage($im, imagick::COMPOSITE_OVER, 20, 10);
+$canvas->compositeImage($reflection, imagick::COMPOSITE_OVER, 20, $im->getImageHeight() + 10);
+
+/* Вывод изображения */
+header("Content-Type: image/png");
+echo $canvas;
+
+?>
