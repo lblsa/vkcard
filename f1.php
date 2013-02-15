4ee931b7ea44afb24a1430e57a888ae8d9112ab1
vinilzen <marchukilya@gmail.com>
2013-02-15 21:00:27 +0100

    test font antialiasing

---
+++ f1.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Создаём объект imagickdraw */
+$draw = new ImagickDraw();
+
+/* Устанавливаем размер шрифта в 52 */
+$draw->setFontSize(80);
+
+/* Добавляем свой текст */
+$draw->annotation(20, 50, "Hello World!");
+
+/* Создаём новый объект холста и белое изображение */
+$canvas = new Imagick();
+$canvas->newImage(350, 70, "white");
+
+/* Рисуем ImagickDraw на холсте */
+$canvas->drawImage($draw);
+
+/* Устанавливаем формат PNG */
+$canvas->setImageFormat('png');
+
+/* устанавливаем чёрную рамку шириной 1px вокруг изображения */
+$canvas->borderImage('black', 1, 1);
+
+
+/* Вывод изображения */
+header("Content-Type: image/png");
+echo $canvas;
+
+die;
+?>
---
+++ f_tnr_a.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Создаём объект imagickdraw */
+$draw = new ImagickDraw();
+
+/* Устанавливаем размер шрифта в 52 */
+
+$draw->setStrokeAntialias(true);
+$draw->setFont('tnr.ttf');
+$draw->setFontSize(80);
+
+/* Добавляем свой текст */
+$draw->annotation(20, 50, "Hello World!");
+
+/* Создаём новый объект холста и белое изображение */
+$canvas = new Imagick();
+$canvas->newImage(350, 70, "white");
+
+/* Рисуем ImagickDraw на холсте */
+$canvas->drawImage($draw);
+
+/* Устанавливаем формат PNG */
+$canvas->setImageFormat('jpg');
+
+/* устанавливаем чёрную рамку шириной 1px вокруг изображения */
+$canvas->borderImage('black', 1, 1);
+
+
+/* Вывод изображения */
+header("Content-Type: image/jpg");
+echo $canvas;
+
+die;
+?>
